3b47dbbdff41fa577bfe566b5cec9ecc6e1386a9
Madeline Sabino <93164849+MADGRISMAD@users.noreply.github.com>
2022-04-14 18:48:12 -0700

    Add files via upload

---
+++ ITT/ESTRUCUTRA DE DATOS/programa 17/programa 17/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_17
+{
+    class Program
+    {
+        public class colas
+        {
+            int Max, Frente, Final, Apuntador;
+            float[] cola;
+            public colas(int tamaño)
+            {
+                Max = tamaño;
+                Frente = -1;
+                Final = -1;
+                cola = new float[tamaño];
+                Console.WriteLine("la cola ha sido creada con exito.");
+                Console.WriteLine("presione <enter> para continuar.");
+            }
+
+            public void encolar(float elemento)
+            {
+                if (Frente=0 || Final=(Max-1))
+                {
+                    Console.WriteLine("la cola esta llena");
+
+                }
+                else
+                {
+                    if (Frente = -1)
+                    {
+                        Frente=0;
+                        Final = 0;
+
+                    }
+                    else
+                    {
+                        Final = Final + 1;
+
+                    }
+                    cola[Final] = elemento;
+                }
+            }
+            public void desencolar()
+            {
+                if (Frente!=-1)
+                {
+                    Console.WriteLine("eliminando el dato :" + cola[Frente]);
+                    cola[Frente] ="";
+                    if (Frente = Final)
+                    {
+                        Frente = -1;
+                        Final = -1;
+                    }
+                    else
+                    {
+                        Frente = Frente + 1;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("la cola esta vacia");
+                }
+            }
+
+            public void recorrido()
+            {
+                if (Frente!=-1)
+                {
+                    Apuntador = Frente;
+                    while (Apuntador<=Final)
+                    {
+                        Console.WriteLine("elemento" + cola[Apuntador] + "posicion" + Apuntador);
+                        Apuntador = Apuntador + 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("la cola esta vacia");
+                }
+            }
+
+            public void busqueda(float elemento)
+            {
+                if (Frente!=-1)
+                {
+                    Apuntador = Frente;
+                    while (Apuntador<= Final)
+                    {
+                        if (elemento=Cola[Apuntador])
+                        {
+                            Console.WriteLine("dato encontrado en la posicion: " + Apuntador);
+                            return;
+                        }
+                        Apuntador = Apuntador - 1;
+                    }
+                    Console.WriteLine("dato " + elemento + " no encontrado en la cola");
+                }
+                else
+                {
+                    Console.WriteLine("la cola esta vacia");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ITT/ESTRUCUTRA DE DATOS/programa14-mi ejemplo recursivo/programa14-mi ejemplo recursivo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _14
+{
+    class Program
+    {
+
+        class ciudad
+        {
+            /*md*/
+            public int rec(int poblacion, int ratio, int dias)
+            {
+
+                if (dias == 0)
+                {
+                    return poblacion;
+
+                }
+                else
+                {
+                    
+                    return rec(poblacion, ratio, (dias - 1)) - rec(poblacion, ratio, (dias - 1)) * ratio / 100;
+
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+            int opc;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("determinar la poblacion sana por semana en base a tasa de contagios por dia\n");
+                Console.WriteLine("MENÚ");
+                Console.WriteLine("1.- calcular tasa de contagios");
+                Console.WriteLine("2.- Salir del programa");
+                Console.WriteLine("Escriba que opcion quiere: ");
+                opc = Int32.Parse((Console.ReadLine()));
+
+                switch (opc)
+                {
+
+
+                    case 1:
+
+                        ciudad obb = new ciudad();
+                        Console.WriteLine("\n\nCAPTURA DE DATOS");
+                        Console.WriteLine("poblacion ");
+                        int a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("porcentaje de contagio por dia ");
+                        int b = int.Parse(Console.ReadLine());   
+                        
+
+                        Console.WriteLine("el numero de personas sanas al cabo de " + 7 + " dias sera de " + obb.rec(a, b, 7) + " personas ") ;
+                        /*****/
+                        Console.WriteLine("Presione cualquier tecla para continuar");
+                        Console.ReadKey();
+                        break;
+
+                    case 2:
+                        stopwatch.Stop();
+                        Console.WriteLine("\n\n\nRESULTADO DE LAS COMPLEJIDADES DE ESPACIO Y TIEMPO");
+                        long ticks1 = stopwatch.ElapsedTicks;
+                        TimeSpan ts = TimeSpan.FromTicks(ticks1);
+                        double minutes = ts.TotalMinutes;
+                        Console.WriteLine(ts + " segundos transcurridos");
+
+                        Console.WriteLine("\nMemoria total: {0} bytes", GC.GetTotalMemory(false));
+                        /*****/
+                        Console.WriteLine("\nPresione cualquier tecla para terminar...");
+
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion invalida, intente otra vez.");
+                        Console.ReadKey();
+                        break;
+
+                }
+                
+            } while (opc != 2);
+        }
+    }
+}
