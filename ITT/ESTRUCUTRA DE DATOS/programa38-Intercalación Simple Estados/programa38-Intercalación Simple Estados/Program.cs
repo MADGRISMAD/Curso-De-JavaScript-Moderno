97fe76f1a04b59cbd1398cf99a7633612f09782d
Madeline Sabino <93164849+MADGRISMAD@users.noreply.github.com>
2022-05-22 02:50:43 -0700

    Add files via upload

---
+++ ITT/ESTRUCUTRA DE DATOS/programa38-Intercalación Simple Estados/programa38-Intercalación Simple Estados/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa38_Intercalación_Simple_Estados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
